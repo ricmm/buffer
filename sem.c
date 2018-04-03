1835287574a7dac8cf8f255d1b66d29141f35c0a
Ricardo Mendoza <mendoza.ricardo@gmail.com>
2018-04-03 18:17:36 +0200

    enable muslc use case

--- sem.c
+++ sem.c
@@ -41,7 +41,7 @@
  *
  * mbuck@debian.org, 1999/08/29
  */
-#if defined(__GNU_LIBRARY__) && defined(_SEM_SEMUN_UNDEFINED)
+#if defined(_SEM_SEMUN_UNDEFINED)
 union semun {
 	int val;			/* value for SETVAL              */
 	struct semid_ds *buf;		/* buffer for IPC_STAT & IPC_SET */
